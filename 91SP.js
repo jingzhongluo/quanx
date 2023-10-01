44be92525bdb32accd19d65e4a820d9e01ed0952
QUQU <64067621+jingzhongluo@users.noreply.github.com>
2023-10-01 17:35:12 +0800

    Update 91SP.js

--- 91SP.js
+++ 91SP.js
@@ -1,1 +1,10 @@
+#====Script====#
 
+# gg
+# hostname = isi.*.g*.com
+^https:\/\/isi\..*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body http://git.yycm.link/chxm1023/Rewrite/raw/main/gg.js
+^https:\/\/isi\..*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header http://git.yycm.link/chxm1023/Rewrite/raw/main/gg.js
+
+#*****************#
+  
+hostname = isi.*.g*.com
